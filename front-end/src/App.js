8ec1447d09d14f1aa650cd0251bdedec9332ddd8
chituenguyen <65612785+chituenguyen@users.noreply.github.com>
2021-08-23 23:08:03 +0700

    Update App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div>Hello World</div>;
+  return <div>Hello World, hello my friends</div>;
 }
 
 export default App;
