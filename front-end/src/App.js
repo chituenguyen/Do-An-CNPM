2486b6090897cefe4543522ee3acc98f552d95bb
chituenguyen <tuenguyen553@gmail.com>
2021-08-23 23:22:48 +0700

    branch tue

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div>Hello World, hello my friends,i am tue im good </div>;
+  return <div>Hello World, hello my friends,i am tue im good, branch tue </div>;
 }
 
 export default App;
