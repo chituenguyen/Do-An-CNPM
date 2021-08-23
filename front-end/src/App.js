5a02e7c3ab8ba9d9607a01650de44b2519bc13df
chituenguyen <tuenguyen553@gmail.com>
2021-08-23 23:32:53 +0700

    create project

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div>Hello World, hello my friends,i am tue im good, branch tue </div>;
+  return <div>Hello World</div>;
 }
 
 export default App;
