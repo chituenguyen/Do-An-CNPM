8978a1f0488c2f3cda3557aa68ed2eb002bd3f53
chituenguyen <tuenguyen553@gmail.com>
2021-09-06 16:27:28 +0700

    test

--- front-end/src/Screen/HomeScreen.js
+++ front-end/src/Screen/HomeScreen.js
@@ -10,7 +10,7 @@ function HomeScreen() {
       <Row>
         {products.map((product) => (
           <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product}></Product>
+            <Product product={product}>123</Product>
           </Col>
         ))}
       </Row>
