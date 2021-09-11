54c785b1d26a27a04951bae0cd4b522192898464
chituenguyen <tuenguyen553@gmail.com>
2021-09-11 20:19:10 +0700

    none

--- front-end/src/Screen/ProductScreen.js
+++ front-end/src/Screen/ProductScreen.js
@@ -1,21 +1,140 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { Row, Col, Image, ListGroup, Button, Card } from "react-bootstrap";
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Button,
+  Card,
+  Form,
+} from "react-bootstrap";
 import Rating from "../components/Rating";
 import products from "../products";
 
 function ProductScreen({ match }) {
+  console.log(products);
   console.log(match);
+  const [qty, setQty] = useState(0);
   const product = products.find((p) => p._id === match.params.id);
+  console.log(product);
   return (
     <div>
-      {product.name}
-      <br></br>
-      {product.price}
-      <br></br>
-      {product.description}
-      <br></br>
-      {product.countInStock}
+      {/* <Row>
+        <Col md={6}>
+          <Image src={product.image}></Image>
+        </Col>
+        <Col>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+                color={"#f8e825"}
+              ></Rating>
+            </ListGroup.Item>
+            <ListGroup.Item>Price : ${product.price}</ListGroup.Item>
+            <ListGroup.Item>{product.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col>
+          <ListGroup>
+            <ListGroup.Item>Price : ${product.price}</ListGroup.Item>
+            <ListGroup.Item>
+              Status :{" "}
+              {product.countInStock > 0 ? "In stock" : "Out of in stock"}
+            </ListGroup.Item>
+            <ListGroup.Item></ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row> */}
+
+      {/* <Row>
+        <Col md={6}>
+          <Image
+            src={product.image}
+            alt={product.name}
+            className="imageProduct"
+          ></Image>
+        </Col>
+
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+                color={"#f8e825"}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item>Price:${product.price}</ListGroup.Item>
+            <ListGroup.Item>Description:{product.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+
+        <Col md={3}>
+          <Card>
+            <ListGroup>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    <strong>
+                      {product.countInStock > 0 ? "Instock" : "Out of stock"}
+                    </strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+
+              {product.countInStock > 0 && (
+                <ListGroup.Item>
+                  <Row xs="auto" className="my-1">
+                    <Col>Quantity:</Col>
+                    <Col>
+                      <Form.Control
+                        as="select"
+                        defaultValue={qty}
+                        onChange={(e) => {
+                          setQty(e.target.value);
+                        }}
+                      >
+                        {[...Array(product.countInStock).keys()].map((x) => (
+                          <option value={x + 1} key={x + 1}>
+                            {x + 1}
+                          </option>
+                        ))}
+                      </Form.Control>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+              )}
+
+              <ListGroup.Item>
+                <Button
+                  className="btn-block"
+                  disabled={product.countInStock == 0}
+                >
+                  Add to cart
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row> */}
     </div>
   );
 }
