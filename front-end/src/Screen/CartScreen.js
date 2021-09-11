eb33043047f928f63986d6a0542376d85dbf1bcb
chituenguyen <tuenguyen553@gmail.com>
2021-09-11 20:58:26 +0700

    fix

--- front-end/src/Screen/CartScreen.js
+++ front-end/src/Screen/CartScreen.js
@@ -7,84 +7,78 @@ import {
   Button,
   Card,
   Form,
-  Message,
-  Container,
   ListGroupItem,
-  Alert,
 } from "react-bootstrap";
 
-import '../CartScreen.css'
-import Rating from "./../components/Rating";
 import products from "../products";
+
+import { Link } from "react-router-dom";
 
 function CartScreen() {
   return (
-  <div className="cartScreen">
-        <Container>
-        <h1 className="heading">Shopping Cart</h1>
-          <Row>
-            <Col lg={8} md={12} sm={12}>
-              { 
-              products.map((product) =>(
-                <Row className="product__item" key={product._id}>
+    <div>
+      <Link to="/" className="btn btn-light my-3">
+        Go Back
+      </Link>
+      <Row>
+        <Col md={8}>
+          <h1>Shopping Cart</h1>
+          <ListGroup variant="flush">
+            {products.map((product) => (
+              <ListGroup.Item variant="flush">
+                <Row>
+                  <Col md={2}>
+                    <Image
+                      src={`${product.image}`}
+                      style={{
+                        width: "60px",
+                      }}
+                      rounded
+                    />
+                  </Col>
 
-                <Col className="text-center">
-                <Image 
-                src={`${product.image}`} 
-                style ={{
-                  width:'60px'
-                }} rounded />
-                </Col>
+                  <Col md={3}>{product.name}</Col>
 
-                <Col className="text-center">
-                {product.name}
-                </Col>
-                
-                <Col className="text-center">
-                ${product.price} 
-                </Col>
+                  <Col md={2}>${product.price}</Col>
 
-                <Col className="text-center">
-                <form className="product__number">
-                  <select>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                  </select>
-                </form>
-                  
-               
+                  <Col md={2}>
+                    <Form.Control as="select" defaultValue={1}>
+                      {[...Array(product.countInStock).keys()].map((x) => (
+                        <option value={x + 1} key={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
+                    </Form.Control>
+                  </Col>
 
-                </Col>
-                
-                <Col className="text-center">
-                <i class="fas fa-trash"></i>
-                </Col>
-              </Row>
-              ))
-              }
-              </Col>
-            <Col lg={4} md={12} sm ={12}>
-              <Card>
-               <Card.Body>
-                 <Card.Text className="cart__proceed-total">
-                   SUBTOTAL (5) ITEMS
-                 </Card.Text>
-                 <Card.Text>
-                   $1000
-                 </Card.Text>
-               </Card.Body>
-              </Card>
-              <Card.Footer className="btn__footer">
-                <Button className="btn__checkout">PROCEDD TO CHECKOUT</Button>
-              </Card.Footer>
-            </Col>
-          </Row>
-        </Container>      
-  </div>
-  )
+                  <Col md={1}>
+                    <Button type="button" variant="light">
+                      <i className="fas fa-trash"></i>
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        </Col>
+        <Col md={4}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroupItem>
+                <h2>SubTotal (5) items</h2>
+                $1000
+              </ListGroupItem>
+              <ListGroupItem className="d-grid gap-2">
+                <Button type="button" className="btn-block">
+                  Process To Checkout
+                </Button>
+              </ListGroupItem>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
 export default CartScreen;
