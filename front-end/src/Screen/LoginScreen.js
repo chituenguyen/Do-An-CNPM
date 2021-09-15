b2f8ec7f03c72bda34c59ba16645d9aea00ee41e
chituenguyen <tuenguyen553@gmail.com>
2021-09-15 16:25:33 +0700

    loginScreen

--- front-end/src/Screen/LoginScreen.js
+++ front-end/src/Screen/LoginScreen.js
@@ -1,8 +1,30 @@
 import React from "react";
-import { Form, Button, Row, Col, Alert } from "react-bootstrap";
+import { Form, Button, Row, Col } from "react-bootstrap";
 import FormContainer from "../components/FormContainer";
-import { useState, useEffect } from "react";
-import { Link, Redirect } from "react-router-dom";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Styles = styled.div`
+  h1{
+    margin-top:20px;
+    margin-bottom:40px;
+    font-size:200px
+    background:#000754;
+  }
+  #email {
+    margin-bottom: 20px;
+  }
+  #footer{
+    
+  }
+  Button{
+    margin-top:30px;  
+  }
+  .py-3{
+    margin-top:20px;
+  }
+`;
 
 function LoginScreen() {
   const [email, setEmail] = useState("");
@@ -11,41 +33,44 @@ function LoginScreen() {
     e.preventDefault();
     console.log({ email }, { password });
   };
+
   return (
     <FormContainer>
-      <h1>Sign In</h1>
-      <Form onSubmit={handleSubmit}>
-        <Form.Group controlId="email">
-          <Form.Label>Email Adress</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
+      <Styles>
+        <h1>Sign In</h1>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="email">
+            <Form.Label>Email Adress</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
 
-        <Form.Group controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Enter password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            name="password"
-          ></Form.Control>
-        </Form.Group>
+          <Form.Group controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              name="password"
+            ></Form.Control>
+          </Form.Group>
 
-        <Button variant="primary" type="submit">
-          Enter
-        </Button>
-      </Form>
-      <Row className="py-3">
-        <Col>
-          New Customer?
-          <Link to={"/register"}>Register</Link>
-        </Col>
-      </Row>
+          <Button variant="primary" type="submit">
+            Sign in
+          </Button>
+        </Form>
+        <Row className="py-3">
+          <Col id="footer">
+            New Customer?
+            <Link to={"/register"}>Register</Link>
+          </Col>
+        </Row>
+      </Styles>
     </FormContainer>
   );
 }
